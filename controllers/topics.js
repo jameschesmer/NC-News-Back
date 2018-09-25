a76616c5be13c14f4e1b6b8d7f1c6b439a405d4c
jameschesmer <jameschesmer@gmail.com>
2018-09-25 11:46:59 +0100

    added ability to get comment count for articles by topic

--- controllers/topics.js
+++ controllers/topics.js
@@ -1,5 +1,7 @@
 const Topic = require('../models/Topic');
 const Article = require('../models/Article');
+const Comment = require('../models/Comment');
+
 
 const getTopics = (req, res, next) => {
   let query = {};
@@ -18,9 +20,28 @@ const getArticlesByTopic = (req, res, next) => {
   let query = {};
   if (req.params.topic_slug) query = { belongs_to: req.params.topic_slug };
   return Article.find(query)
+    .lean()
     .then(articles => {
+      const commentsPerArticle = articles.map(article => {
+        return Comment.count({ belongs_to: article._id });
+      });
+      return Promise.all([
+        articles,
+        Promise.all(commentsPerArticle)
+      ]);
+    })
+    .then(([articles, comment_count]) => {
       if (!articles[0]) return Promise.reject({ status: 404, msg: 'Page Not Found' });
-      else res.status(200).send({ articles });
+      if (articles.length === 1) {
+        articles = articles[0];
+        res.status(200).send({ articles: { ...articles, comment_count: comment_count[0] } });
+      } else {
+        const obj = [];
+        for (let i = 0; i < articles.length; i++) {
+          obj.push({ ...articles[i], comment_count: comment_count[i] });
+        }
+        res.status(200).send({ articles: obj });
+      }
     })
     .catch(err => {
       if (err.name === 'CastError') next({ status: 400, msg: 'Bad Request' });
